a923096ab682561eb59ab1dc8937b792b8c7b19a
blade-demon <xuziwei89@gmail.com>
2019-01-06 14:38:40 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# MERN
+
+使用 MongoDB，Express, React, Nodejs 技术栈开发的全栈开发环境，代码部署在 Heroku。
---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send({
+    hi: "there"
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, err => {
+  if (!err) {
+    console.log(`Server is running on port: ${PORT}.`);
+  } else {
+    console.log(`Failed to start the server: ${JSON.stringify(err)}`);
+  }
+});
