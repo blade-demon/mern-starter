d812a9e3f88ccd8adc620706b64493644b2c8850
blade-demon <xuziwei89@gmail.com>
2019-01-06 19:17:13 +0800

    add google oauth

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  googleClientID:
+    "517110964743-auoah64451bsmm7blnpeaj9gsgum4dtu.apps.googleusercontent.com",
+  googleClientSecret: "uqPnplFIFOH2BveTDLNQM8Ct"
+};
--- index.js
+++ index.js
@@ -1,9 +1,48 @@
-const express = require('express');
+const express = require("express");
+const HttpsProxyAgent = require("https-proxy-agent");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("./config/keys");
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ bye: 'buddy' });
+if (process.env["https_proxy"]) {
+  options.agent = new HttpsProxyAgent(process.env["https_proxy"]);
+}
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback"
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log("access token", accessToken);
+      console.log("refresh token", refreshToken);
+      console.log("profile", profile);
+    }
+  )
+);
+
+app.get("/", (req, res) => {
+  res.send({ bye: "buddy" });
 });
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile"]
+  })
+);
+
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", { failureRedirect: "/" }, function(req, res) {
+    // Successful authentication, redirect home.
+    console.log(res);
+    res.send("ok");
+  })
+);
 
 const PORT = process.env.PORT || 3000;
 
