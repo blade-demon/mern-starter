f8a657e0d8692eebd087fbc80b9fa5496ae626c8
blade-demon <xuziwei89@gmail.com>
2019-07-20 00:32:07 +0800

    add yarn to install

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
-const express = require("express");
-const keys = require("./config/keys");
-const mongoose = require("mongoose");
-const cookieSession = require("cookie-session");
-const passport = require("passport");
+const express = require('express');
+const keys = require('./config/keys');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 
-require("./models/User");
-require("./services/passport");
+require('./models/User');
+require('./services/passport');
 const app = express();
 
 app.use(
-  cookieSession({
-    maxAge: 24 * 3600 * 1000 * 30,
-    keys: [keys.cookieKey]
-  })
+	cookieSession({
+		maxAge: 24 * 3600 * 1000 * 30,
+		keys: [keys.cookieKey],
+	})
 );
 
 app.use(passport.initialize());
 app.use(passport.session());
 
-require("./routes/authRoutes")(app);
+require('./routes/authRoutes')(app);
 
-mongoose.connect(keys.mongoURI);
+mongoose.connect(keys.mongoURI, {useNewUrlParser: true});
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, err => {
-  if (!err) {
-    /* eslint no-console: 0*/
-    console.log(`Server is running on port: ${PORT}.`);
-  } else {
-    /* eslint no-console: 0*/
-    console.log(`Failed to start the server: ${JSON.stringify(err)}`);
-  }
+	if (!err) {
+		/* eslint no-console: 0*/
+		console.log(`Server is running on port: ${PORT}.`);
+	} else {
+		/* eslint no-console: 0*/
+		console.log(`Failed to start the server: ${JSON.stringify(err)}`);
+	}
 });
