7ff963d45a6c733aba5fbd7dcc8a25a4bacb50a6
blade-demon <xuziwei89@gmail.com>
2019-07-25 00:29:51 +0800

    add redux thunk

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,26 +1,38 @@
-import React from "react";
+import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { connect } from "react-redux";
+import * as actions from "../actions";
+
 import Header from "./Header";
 import Landing from "./Landing";
 import Dashboard from "./Dashboard";
 
 const Surveys = () => <h1>Surveys</h1>;
 
-function App() {
-	return (
-		<div className='App'>
-			<header className='App-header'>
-				<Header />
-				<Router>
-					<div>
-						<Route exact path='/' component={Landing} />
-						<Route exact path='/surveys' component={Dashboard} />
-						<Route exact path='/surveys/new' component={Surveys} />
-					</div>
-				</Router>
-			</header>
-		</div>
-	);
+class App extends Component {
+	componentDidMount() {
+		this.props.fetchUser();
+	}
+
+	render() {
+		return (
+			<div className='App'>
+				<header className='App-header'>
+					<Header />
+					<Router>
+						<div>
+							<Route exact path='/' component={Landing} />
+							<Route exact path='/surveys' component={Dashboard} />
+							<Route exact path='/surveys/new' component={Surveys} />
+						</div>
+					</Router>
+				</header>
+			</div>
+		);
+	}
 }
 
-export default App;
+export default connect(
+	null,
+	actions
+)(App);
--- client/src/index.js
+++ client/src/index.js
@@ -1,23 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {createStore, applyMiddleware} from 'redux';
-import {Provider} from 'react-redux';
-import reducers from './reducers';
-import 'materialize-css/dist/css/materialize.min.css';
-import './index.css';
-import App from './components/App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import reduxThunk from "redux-thunk";
+import reducers from "./reducers";
 
-const store = createStore (reducers, {}, applyMiddleware ());
+import "materialize-css/dist/css/materialize.min.css";
+import "./index.css";
+import App from "./components/App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render (
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById ('root')
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
+
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister ();
+serviceWorker.unregister();
