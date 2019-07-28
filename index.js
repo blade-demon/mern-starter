edefa68341d584eabb6348b28a738e33ab08b039
blade-demon <xuziwei89@gmail.com>
2019-07-28 23:58:16 +0800

    add new command line for heroku deployment

--- index.js
+++ index.js
@@ -24,6 +24,17 @@ require("./routes/billingRoutes")(app);
 
 mongoose.connect(keys.mongoURI, { useNewUrlParser: true });
 
+if (process.env.NODE_ENV === "production") {
+	// Express will serve up production assets like our main.js file or main.css file!
+	app.use(express.static("client/build"));
+
+	// Express will serve up index.html file if it doesn't recognize the route!
+	const path = require("path");
+	app.get("*", (req, res) =>
+		res.send(path.join(__dirname, "client", "build", "index.html"))
+	);
+}
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, err => {
