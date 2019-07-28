9fce2c1210134ef09fd37335bc87b39463a59798
blade-demon <xuziwei89@gmail.com>
2019-07-29 00:16:06 +0800

    fix bugs for serve index.html

--- index.js
+++ index.js
@@ -30,9 +30,9 @@ if (process.env.NODE_ENV === "production") {
 
 	// Express will serve up index.html file if it doesn't recognize the route!
 	const path = require("path");
-	app.get("*", (req, res) =>
-		res.send(path.resolve(__dirname, "client", "build", "index.html"))
-	);
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+	});
 }
 
 const PORT = process.env.PORT || 5000;
